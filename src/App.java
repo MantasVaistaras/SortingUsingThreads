56c8d53f858d05f632e9f62941c7e10750683487
Manteliz <mantasvaist@gmail.com>
2019-11-21 12:41:52 +0200

    Completed the Quick Sort Algorithm

--- src/App.java
+++ src/App.java
@@ -6,11 +6,11 @@ public class App {
 
 	public static void main(String[] args) {	
 		String threadCntString = "";
-		String sourceFile = "";
+		String file = "";
 		String outputFile;
 		try {
 			threadCntString = args[0];
-			sourceFile = args[1];
+			file = args[1];
 			outputFile = args[2];
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Please provide 3 parameters in the command line - number of threads, source file name, output file name.");
@@ -23,8 +23,14 @@ public class App {
 		} catch (NumberFormatException e) {
 			System.out.println("First parameter must be provided in the number format.");
 		}
-		
-		readFile(sourceFile);
+		SourceFile sourceFile = readFile(file);
+		String[] list = sourceFile.getWordList();
+		QuickSort(0, list.length-1, list);
+		for (int i = 0; i < list.length; i++) {
+			String string = list[i];
+			System.out.println(string);
+		}
+
 	}
 	
 	public static SourceFile readFile(String fileName) {
@@ -41,29 +47,36 @@ public class App {
 		}
 		return new SourceFile(wordCount, list);
 	}
-
-	public String[] QuickSort(String[] wordList) {
-		// implementing Quick Sort algorithm
-		
-		int n = wordList.length;
-		String pivot = wordList[0];
-		int i = 0;
-		int j = n;
+	
+	public static void QuickSort(int low, int high, String[] wordList) {
+		// implementing Quick Sort algorithm		
+		String pivot = wordList[high];
+		int i = low - 1;
 		String temp;
-		while(j > i) {
-			do {
-				i++;
-			} while (pivot.compareTo(wordList[i]) > 0);
-			do {
-				j--;
-			} while (pivot.compareTo(wordList[j]) < 0);
-			temp = wordList[j];
-			wordList[j] = wordList[i];
-			wordList[i] = temp;
+		if((high - low) > 0) {
+			//there is more than one element in the partitioning array
+			for(int j = low; j < high; j++) {
+				if(wordList[j].compareTo(pivot) < 0) {
+					//wordList[j] is lexicographically preceding the pivot
+					i++;
+					temp = wordList[i];
+					wordList[i] = wordList[j];
+					wordList[j] = temp;
+				}			
+			}
+			//found the pivot position = i+1. Every element after the index i must be equal or bigger than the pivot
+			temp = wordList[i+1];
+			wordList[i+1] = pivot;
+			wordList[high] = temp;
+			if((i+1-low) >= 2) {
+				//there are more than one element to the left of the pivot
+				QuickSort(low, i, wordList);
+			}
+			if((high - (i+1)) >= 2) {
+				//there are more than one element to the right of the pivot
+				QuickSort(i+2, high, wordList);
+			}
 		}
-		temp = wordList[j];
-		wordList[j] = pivot;
-		wordList[0] = temp;
-		
+	
 	}
 }
